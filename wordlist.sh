070d5062fb1e6df550f953621893f2d98fcdf8aa
Luka Pušić <pusic93@gmail.com>
2012-01-12 23:28:39 +0100

    Added more restricted image formats, replaced ./wordlist.sh to  for stdout

--- wordlist.sh
+++ wordlist.sh
@@ -10,7 +10,7 @@ then
 	echo ">> $(date +'%T') - Starting to downlad $1. This can take a long time..."
 
 	#download websites recursively to ./temp/ directory, skip non-text files
-	wget -r -l 2 --random-wait --user-agent='Mozilla/5.0' --quiet -R .jpg,.jpeg,.png,.gif,.bmp,.flv,.js,.avi,.wmv,.mp3,.zip,.css,.pdf,.iso,.tar.gz,.rar,.swf -P "./temp/" $1
+	wget -r -l 2 --random-wait --user-agent='Mozilla/5.0' --quiet -R .jpg,.jpeg,.png,.gif,.bmp,.flv,.js,.avi,.wmv,.mp3,.zip,.css,.pdf,.iso,.tar.gz,.rar,.swf,.PNG,.GIF,.JPG,.JPEG,.BMP -P "./temp/" $1
 
 	echo ">> $(date +'%T') - Finished downloading, creating wordlist..."
 
@@ -27,7 +27,7 @@ then
 		echo ">> Wordlist merged with $2 and now has $(cat "$2" | wc -l) lines!";
 	else
 		echo "$page" > "$2"
-		echo ">> Wordlist saved to ./wordlist.txt!"
+		echo ">> Wordlist saved to $2!"
 	fi
 
 	#remove temporaray website directory
